6c448f922494ce7bfa67c7861090920960e9e1d9
Deepak <ds6808542@gmail.com>
2026-04-14 16:37:18 +0530

    asgi  changed

--- projectchat/projectchat/asgi.py
+++ projectchat/projectchat/asgi.py
@@ -26,30 +26,30 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
-# import app1.routing 
+import app1.routing 
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectchat.settings')
-
-# application = ProtocolTypeRouter({
-#     "http": get_asgi_application(),
-#     "websocket": AuthMiddlewareStack(
-#         URLRouter(
-#             app1.routing.websocket_urlpatterns
-#         )
-#     ),
-# })
-
-from projectchat import app1
-import projectchat.app1.routing 
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectchat.projectchat.settings')
-django.setup()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectchat.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            projectchat.app1.routing.websocket_urlpatterns
+            app1.routing.websocket_urlpatterns
         )
     ),
 })
+
+# from projectchat import app1
+# import projectchat.app1.routing 
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectchat.projectchat.settings')
+# django.setup()
+
+# application = ProtocolTypeRouter({
+#     "http": get_asgi_application(),
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             projectchat.app1.routing.websocket_urlpatterns
+#         )
+#     ),
+# })
